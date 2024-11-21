08263665bb80556e74e1c9bb764c3c4a12d93bb5
Erick Wendel <erick.workspace@gmail.com>
2024-11-21 18:12:59 -0300

    add in class demos
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,27 @@
 import _ from 'lodash'
 import { createServer } from 'node:http'
+import { Session } from 'node:inspector/promises'
+import { writeFile } from 'node:fs/promises'
+function cpuProfiling() {
+    let _session
+    return {
+        async start() {
+            _session = new Session()
+            _session.connect()
 
+            await _session.post('Profiler.enable')
+            await _session.post('Profiler.start')
+            console.log('started CPU Profiling')
+        },
+        async stop() {
+            console.log('stopping CPU Profiling')
+            const { profile } = await _session.post('Profiler.stop')
+            const profileName = `cpu-profile-${Date.now()}.cpuprofile`
+            await writeFile(profileName, JSON.stringify(profile))
+            _session.disconnect()
+        },
+    }
+}
 
 const largeDataset = Array.from({ length: 1e4 }, (_, id) => ({
     id,
@@ -10,13 +31,12 @@ const largeDataset = Array.from({ length: 1e4 }, (_, id) => ({
 
 function issueRoute() {
     const clonedData = _.cloneDeep(largeDataset);
-
-    const activeUsers = _.filter(clonedData, { isActive: true });
-
-    const transformedUsers = _.map(activeUsers, (user) => ({
+    const activeUsers = clonedData.filter((item) => item.isActive);
+    const transformedUsers = activeUsers.map((user) => ({
         ...user,
         name: user.name.toUpperCase(),
     }));
+
     return transformedUsers;
 }
 
@@ -52,3 +72,14 @@ createServer(
     .once('listening', function onListening() {
         console.log('Server started on http://localhost:3000');
     });
+
+const { start, stop } = cpuProfiling()
+start()
+
+const exitSignals = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+exitSignals.forEach(signal => {
+    process.once(signal, async () => {
+        await stop()
+        process.exit(0)
+    })
+})
